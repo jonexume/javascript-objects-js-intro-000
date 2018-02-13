b0a1c60046318acbb82aecd5965368b92b84435d
Jon Exume <jonexume@gmail.com>
2018-02-13 20:13:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistNames: "song title"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle});
+  return Object.assign(playlist, {[artistName]: songTitle});
 }
