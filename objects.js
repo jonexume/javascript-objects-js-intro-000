897198e7df71acaf1003fa6e83c0dc8f57e42a33
Jon Exume <jonexume@gmail.com>
2018-02-13 20:23:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName]: songTitle});
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+function removeFromPlaylist(object, artistName) {
+  delete object.artistName;
+  return playlist;
 
-  
+
 }
